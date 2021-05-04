432f8888d1c9fed9f96a17f2423e445c2217ccc7
Robinson Presotto <robipresotto@gmail.com>
2021-05-04 19:52:36 +0200

    Creates the peptides matcher command line tool

---
+++ Sources/VacciBodyLib/Commands/PeptidesMatcher.swift
@@ -0,0 +1,51 @@
+//
+//  ParsableCommand.swift
+//  VacciBodyLib
+//
+//  Created by Robinson Presotto on 30/04/2021.
+//
+
+import ArgumentParser
+
+public struct PeptidesMatcher: ParsableCommand {
+  
+  public init() {}
+  public static let configuration = CommandConfiguration(
+    commandName: "match",
+    abstract: "Scan a list of short peptides against the proteome and returns the matches"
+  )
+
+  @Argument(help: "Peptides file path")
+  private var peptidesFilePath: String
+  
+  @Argument(help: "Proteome file path")
+  private var proteomeFilePath: String
+  
+  public func run() throws {
+    
+    let loading = Loading.init(
+      title: "VacciBody CLI"
+    )
+    
+    try Matcher.runAsync(
+      peptidesFilePath: peptidesFilePath,
+      proteomeFilePath: proteomeFilePath,
+      onProgress: { total, progress, matches in
+        loading.progress(
+          progress: progress,
+          total: total,
+          matches: matches
+        )
+      },
+      onComplete: { matches in
+        loading.done(maches: matches.count)
+         try ParserFastaFile.writeData(
+          data: matches
+        )
+      }
+    )
+    
+  }
+  
+}
+
