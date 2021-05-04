c3db4df714630c866fd3754a66fe3d70f7ae4b8d
Robinson Presotto <robipresotto@gmail.com>
2021-05-04 19:42:36 +0200

    Update package manager dependencies

--- Package.swift
+++ Package.swift
@@ -4,19 +4,34 @@
 import PackageDescription
 
 let package = Package(
-    name: "VacciBody",
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "VacciBody",
-            dependencies: []),
-        .testTarget(
-            name: "VacciBodyTests",
-            dependencies: ["VacciBody"]),
-    ]
+  name: "VacciBody",
+  products: [
+    .executable(
+      name: "VacciBody",
+      targets: ["VacciBody"]
+    ),
+    .library(
+      name: "VacciBodyLib",
+      targets: ["VacciBodyLib"]
+    )
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-argument-parser", from: "0.4.0"),
+    .package(url: "https://github.com/apple/swift-tools-support-core.git", from: "0.2.0")
+  ],
+  targets: [
+    .target(
+      name: "VacciBody",
+      dependencies: ["VacciBodyLib"]),
+    .target(
+      name: "VacciBodyLib",
+      dependencies: [
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+        .product(name: "SwiftToolsSupport-auto", package: "swift-tools-support-core")
+      ]
+    ),
+    .testTarget(
+      name: "VacciBodyTests",
+      dependencies: ["VacciBodyLib"]),
+  ]
 )
